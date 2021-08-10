6663a020764d2b4c23d06d9bff8dee9cf18156a2
Ucing <33126545+ferlitopym@users.noreply.github.com>
2021-08-10 09:27:08 +0700

    Add files via upload

---
+++ premium help.js
@@ -0,0 +1,69 @@
+╔═════[ *DOWNLOADER* ]
+╠➤ *${p+=1}.* ${prefix}play _query_
+╠➤ *${p+=1}.* ${prefix}playvid _query_
+╠➤ *${p+=1}.* ${prefix}ytmp3 _link_
+╠➤ *${p+=1}.* ${prefix}ytmp4 _link_
+╠➤ *${p+=1}.* ${prefix}igstory _username_
+╠➤ *${p+=1}.* ${prefix}ig _link_
+╠➤ *${p+=1}.* ${prefix}joox _song_
+╚═════[ *PREMIUM BOT* ]
+
+╔═════[ *SEARCHING* ]
+╠➤ *${p+=1}.* ${prefix}playstore _apk_
+╠➤ *${p+=1}.* ${prefix}happymod _apk_
+╠➤ *${p+=1}.* ${prefix}iguser _username_
+╠➤ *${p+=1}.* ${prefix}igstalk _username_
+╠➤ *${p+=1}.* ${prefix}ytsearch _query_
+╠➤ *${p+=1}.* ${prefix}shoope _product_
+╠➤ *${p+=1}.* ${prefix}spotify _song_
+╠➤ *${p+=1}.* ${prefix}gsmarena _hp_
+╠➤ *${p+=1}.* ${prefix}searchmusic _replyAudio_
+╠➤ *${p+=1}.* ${prefix}pinterest _query_
+╠➤ *${p+=1}.* ${prefix}googleimage _query_
+╠➤ *${p+=1}.* ${prefix}jagokata _kata_
+╚═════[ *FREE BOT* ]
+
+╔═════[ *TEXTMAKER* ]
+╠➤ *${p+=1}.* ${prefix}wolflogo _text1|text2_
+╠➤ *${p+=1}.* ${prefix}logoaveng _text1|text2_
+╠➤ *${p+=1}.* ${prefix}phlogo _text1|text2_
+╠➤ *${p+=1}.* ${prefix}marvellogo _text1|text2_
+╠➤ *${p+=1}.* ${prefix}gtext _text1|text2_
+╠➤ *${p+=1}.* ${prefix}pubglogo _text1|text2_
+╠➤ *${p+=1}.* ${prefix}snowwrite _text1|text2_
+╠➤ *${p+=1}.* ${prefix}watercolour _text1|text2_
+╠➤ *${p+=1}.* ${prefix}harta _text_
+╠➤ *${p+=1}.* ${prefix}thundertext _text_
+╠➤ *${p+=1}.* ${prefix}flametext _text_
+╠➤ *${p+=1}.* ${prefix}glowtext _text_
+╠➤ *${p+=1}.* ${prefix}smoketext _text_
+╠➤ *${p+=1}.* ${prefix}lithgtext _text_
+╠➤ *${p+=1}.* ${prefix}flowertext _text_
+╠➤ *${p+=1}.* ${prefix}bneon _text_
+╠➤ *${p+=1}.* ${prefix}matrix _text_
+╠➤ *${p+=1}.* ${prefix}breakwall _text_
+╠➤ *${p+=1}.* ${prefix}gneon _text_
+╠➤ *${p+=1}.* ${prefix}dropwater _text_
+╠➤ *${p+=1}.* ${prefix}tfire _text_
+╠➤ *${p+=1}.* ${prefix}sandw _text_
+╠➤ *${p+=1}.* ${prefix}epep _text_
+╠➤ *${p+=1}.* ${prefix}gplaybutton _text_
+╠➤ *${p+=1}.* ${prefix}splaybutton _text_
+╠➤ *${p+=1}.* ${prefix}text3dbox _text_
+╠➤ *${p+=1}.* ${prefix}text3d _text_
+╠➤ *${p+=1}.* ${prefix}logobp _text_
+╠➤ *${p+=1}.* ${prefix}leavest _text_
+╠➤ *${p+=1}.* ${prefix}tlight _text_
+╠➤ *${p+=1}.* ${prefix}naruto _text_
+╠➤ *${p+=1}.* ${prefix}crosslogo _text_
+╠➤ *${p+=1}.* ${prefix}cslogo _text_
+╠➤ *${p+=1}.* ${prefix}crismes _text_
+╚═════[ *FREE BOT* ]
+
+╔═════[ *IMAGEMAKER* ]
+╠➤ *${p+=1}.* ${prefix}missing _text1|text2|text3|@tag_
+╠➤ *${p+=1}.* ${prefix}calender _replyImage / @tag_
+╠➤ *${p+=1}.* ${prefix}drawing _replyImage / @tag_
+╠➤ *${p+=1}.* ${prefix}sketch _replyImage / @tag_
+╚═════[ *FREE BOT* ]
+
